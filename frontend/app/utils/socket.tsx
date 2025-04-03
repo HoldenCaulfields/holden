69956d4be902d778ba95f5a71ab0895a886a421d
HoldenCaulfields <nguyenthianh1232345@gmail.com>
2025-04-03 11:42:01 +0700

    add websocket in frontend

--- frontend/app/utils/socket.tsx
+++ frontend/app/utils/socket.tsx
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://holden.onrender.com/");
+const socket = io("https://holden.onrender.com/", {
+    transports: ['websocket'],
+});
 
 export default socket; 
